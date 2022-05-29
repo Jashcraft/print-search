d62b9e7b289098e927ff6ed2e5d62ec233db6dad
Jonathon Ashcraft <ashcraft.jonathon@gmail.com>
2022-05-28 19:38:34 -0600

    Moved signing in and creating account to GraphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { UserInputError, AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -14,6 +15,28 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Please log in to continue');
+    }
+  },
+
+  Mutation: {
+    login: async(_, args) => {
+      const {email, password} = args;
+      const user = await User.findOne({email: email});
+      if(!user){
+        throw new AuthenticationError('Wrong Credentials');
+      }
+      const isPasswordCorrect = await user.isCorrectPassword(password);
+
+      if(!isPasswordCorrect){
+        throw new AuthenticationError('Wrong Credentials');
+      }
+      const token = signToken(user);
+      return {token,user}
+    },
+    addUser: async(_, args) => {
+      const user = await User.create(args);
+      const token = signToken(user)
+      return {token, user}
     }
   }
 }
